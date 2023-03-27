f7c08a114de1ba6c7fb2cdc872c61fe4211698d6
danashinnn <103996283+danashinnn@users.noreply.github.com>
2023-03-27 22:18:30 +0900

    Initial commit

---
+++ src/main/java/org/zerock/self_b01/SelfB01Application.java
@@ -0,0 +1,13 @@
+package org.zerock.self_b01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfB01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfB01Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/zerock/self_b01/SelfB01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.self_b01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfB01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
